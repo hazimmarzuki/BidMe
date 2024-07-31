7bd00247ca1513a632a28f977c8b6db4f46d85b0
hazimmarzuki <2021898792@student.uitm.edu.my>
2024-07-31 23:22:48 +0800

    update logout

--- app/Http/Controllers/Auth/LogoutController.php
+++ app/Http/Controllers/Auth/LogoutController.php
@@ -4,13 +4,17 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class LogoutController extends Controller
 {
-    //
-    public function logout(){
+    public function logout()
+    {
+        if (Auth::check()) {
+            Auth::logout();
+            return redirect()->route('show-items-square');
+        }
 
-        auth() -> logout();
         return redirect()->route('show-items-square');
     }
 }
