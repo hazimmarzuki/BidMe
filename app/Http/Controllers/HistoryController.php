f9bc8643039134c8290351e635c60b590581f42b
hazimmarzuki <2021898792@student.uitm.edu.my>
2024-08-01 10:03:30 +0800

    payment

--- app/Http/Controllers/HistoryController.php
+++ app/Http/Controllers/HistoryController.php
@@ -10,20 +10,21 @@ class HistoryController extends Controller
 {
     //
 
-    public function purchasehistory()
-    {
-        $history = Bid::where('buyer_id', Auth::id())
-            ->with(['item', 'seller', 'payment'])
-            ->orderBy('bid_time', 'desc')
-            ->get();
+    public function purchasehistory () {
 
-        $filteredHistory = $history->filter(function ($bid) {
-            return $bid->payment && $bid->payment->bid_id == $bid->id && $bid->payment->status == 'success';
-        });
+        $history =  Bid::where('buyer_id', Auth::id())
+        ->with('item')
+        ->with('seller')
+        ->with('payment')
+        ->orderBy('bid_time', 'desc')
+        ->get();
 
-        return view('purchase-history', ['history' => $filteredHistory]);
+    $filteredHistory = collect($history)->filter(function ($bid) {
+        return $bid->payment && $bid->payment->bid_id == $bid->id && $bid->payment->status == 'success';
+    });
+
+    return view('purchase-history', compact('filteredHistory'));
     }
-
 
     public function saleshistory () {
         $history =  Bid::where('seller_id', Auth::id())
